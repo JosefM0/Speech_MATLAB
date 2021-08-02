9313bec8351174580089e99cdf0344a00fb9e446
JosefM0 <88150529+JosefM0@users.noreply.github.com>
2021-08-02 13:20:50 +0200

    A

--- Segmentation.m
+++ Segmentation.m
@@ -1,4 +1,12 @@
 clear all;
 load('parametrs.mat');
 load('me_ahoj.mat');
-soundsc(speech, Fs);
+% soundsc(speech, Fs);
+
+Tseg = 512; %Pocet vzorku mikrosegmentu 
+
+length(speech);
+
+for k = 1:length(speech)
+    
+end
